6e7e3ffb7ef24cc6255a44bfc8c2268ec59e92b1
Mike <Mhomolak@yahoo.com>
2016-10-31 13:23:00 -0600

    chore: travis

--- npm_scripts/cross-platform.js
+++ npm_scripts/cross-platform.js
@@ -1,7 +1,7 @@
 const fs = require('fs-extra');
 
 try {
-  fs.copySync('/Users/uhomomi/Public/real-component-archetype/component-archetype/node_modules/@pearson-components/npm-scripts/npm_scripts',
+  fs.copySync('/home/travis/build/Pearson-Higher-Ed/component-archetype/node_modules/@pearson-components/npm-scripts/npm_scripts',
   './npm_scripts')
   console.log('success');
 } catch (err) {

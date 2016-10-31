d221c5a895f682ffa8bef3d88eace6d316b38a4c
Mike <Mhomolak@yahoo.com>
2016-10-31 10:23:46 -0600

    fix: Actually copying files

--- npm_scripts/cross-platform.js
+++ npm_scripts/cross-platform.js
@@ -1,6 +1,9 @@
 const fs = require('fs-extra');
 
-fs.copySync('../node_modules/@pearson-components/npm-scripts/npm_scripts/*.js',
-  '../npm_scripts', (err) => {
-  if (err) return console.error(err)
-});
+try {
+  fs.copySync('/Users/uhomomi/Public/real-component-archetype/component-archetype/node_modules/@pearson-components/npm-scripts/npm_scripts',
+  './npm_scripts')
+  console.log('success');
+} catch (err) {
+  console.error(err)
+}

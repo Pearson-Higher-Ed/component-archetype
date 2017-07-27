fec52ae0b820ea3ccdb61244e202ab3da68fd74b
ulantja <Jason.Lantz@pearson.com>
2017-07-26 20:50:07 -0600

    chore: make tests pass

--- test/component-owner.test.js
+++ test/component-owner.test.js
@@ -4,19 +4,17 @@ import React              from 'react';
 import { ComponentOwner } from '../index';
 import { shallow }        from 'enzyme';
 
-describe('Component Owner Suite',() => {
+describe('Component Owner Suite', () => {
 
-  beforeEach(function() {
-    const data = {text:{greeting:"Hi there!", buttonText:"say hello", placeholder: "placeholder"}};
-    this.wrapper = shallow(<ComponentOwner data={data} />);
+  const data = {text:{greeting:"Hi there!", buttonText:"say hello", placeholder: "placeholder"}};
+  const wrapper = shallow(<ComponentOwner data={data} />);
+
+  it('shallowly renders the component', () => {
+    expect(wrapper.node.type).toEqual('div');
   });
 
-  it('shallowly renders the component', function () {
-    expect(this.wrapper.node.type).toEqual('div');
-  });
-
-  it('renders the correct text when the button is clicked', function () {
-    this.wrapper.find('button').simulate('click');
+  it('renders the correct text when the button is clicked', () => {
+    wrapper.find('button').simulate('click');
   });
 
 });

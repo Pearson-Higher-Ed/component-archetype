0f083342e6bfdcac4bbb24a1dfd8e61478db3bd6
Lantz, Jason F <Jason.Lantz@pearson.com>
2018-01-12 09:12:45 -0700

    chore: update to elements-sdk

--- webpack.config.js
+++ webpack.config.js
@@ -6,9 +6,9 @@ const index             = `${__dirname}/index.html`;
 const demo              = `${__dirname}/demo/demo.js`;
 const eventing          = `${__dirname}/demo/eventing.js`;
 const component         = `${__dirname}/index.js`;
-const icons             = `${__dirname}/node_modules/pearson-elements/dist/icons/p-icons-sprite-1.1.svg`;
-const elements          = `${__dirname}/node_modules/pearson-elements/dist/css/elements.css`;
-const fontsDir          = `${__dirname}/node_modules/pearson-elements/dist/fonts/`;
+const icons             = `${__dirname}/node_modules/@pearson-components/elements-sdk/build/icons/p-icons-sprite-1.1.svg`;
+const elements          = `${__dirname}/node_modules/@pearson-components/elements-sdk/build/css/elements.css`;
+const fontsDir          = `${__dirname}/node_modules/@pearson-components/elements-sdk/build/fonts/`;
 const fonts             = fs.readdirSync(fontsDir, 'utf-8').map(font => fontsDir + font);
 
 
